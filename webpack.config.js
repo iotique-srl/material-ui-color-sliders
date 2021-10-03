70a4f8877b21862b14236aa683ffbd7b56155a38
Samuele Dassatti <samuele@igloo.ooo>
2021-10-03 13:09:48 +0200

    Support React 17

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require("path")
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index.js",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, "src"),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  externals: ["react", "react-dom"],
+}
